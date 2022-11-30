72e6f04900a06fdb410d4863449a0188302a4071
Eric Frey <ericlfrey@gmail.com>
2022-11-30 12:51:45 -0600

    Started working on Sort Functionality

--- events/navEvents.js
+++ events/navEvents.js
@@ -7,20 +7,40 @@ const navEvents = (user) => {
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
   document.querySelector('#navBar').addEventListener('click', (e) => {
-    // ADD NEW CARD
-    if (e.target.id === 'showAddCardForm') {
-      addCardForm(user);
-    }
+    // SHOW ALL CARDS
     if (e.target.id === 'logo') {
       getVocabData().then((arr) => {
         cardsOnDOM(arr, user.uid);
       });
     }
+    // ADD NEW CARD
+    if (e.target.id === 'showAddCardForm') {
+      addCardForm(user);
+    }
+    // FILTER CARDS
     if (e.target.id.includes('filter')) {
       const [, language] = e.target.id.split('--');
       getVocabData().then((arr) => {
         const filteredArr = arr.filter((item) => item.category === language);
         cardsOnDOM(filteredArr, user.uid);
+      });
+    }
+    // SORT CARDS
+    if (e.target.id.includes('sort')) {
+      getVocabData().then((arr) => {
+        const titleSort = arr.sort((a, b) => a.title.localeCompare(b.title));
+        // ALPHABETICALLY
+        if (e.target.id === 'sortAlpha') {
+          cardsOnDOM(titleSort, user.uid);
+        }
+        // NEWEST-OLDEST
+        if (e.target.id === 'sortNewest') {
+          console.warn('Newest');
+        }
+        // OLDEST-NEWEST
+        if (e.target.id === 'sortOldest') {
+          console.warn('Oldest');
+        }
       });
     }
   });
